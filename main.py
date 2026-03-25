ef0f0468687b932cb14ce7cc935adb7d466f5e03
Zhengli Sunzhu <1798600@uab.cat>
2026-03-25 12:22:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+min_vots = 200
+num_vots = 600
+avg_item = float
+avg_global = float
 
+def score(num_vots,min_vots,avg_item,avg_global):
+
+  nota = ((num_vots/(num_vots+min_vots))*avg_item)+((num_vots/(num_vots+min_vots))*avg_global)
+  return nota
+def prediccio(min_vots):
